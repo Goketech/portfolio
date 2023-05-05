a3a5ee92fc38fd3b1d789e4cae99fa53d2e2969f
Modupe Akanni <modupe775@gmail.com>
2023-05-05 15:55:29 +0100

    update resume

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -8,7 +8,7 @@ import {FaTwitter} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href='https://www.linkedin.com/in/modupe-akanni-236377235/' target='_blank'><BsLinkedin/></a>
+        <a href='https://www.linkedin.com/in/modupe-akanni/' target='_blank'><BsLinkedin/></a>
         <a href='https://www.github.com/Goketech' target='_blank'><FaGithub/></a>
         <a href="https://twitter.com/aka_goke?t=a61mXAoPDZsPWjzxqTWiWg&s=09" target='_blank'><FaTwitter/></a>
     </div>
