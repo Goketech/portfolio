f9af8b075c650ecd54699940bd1d83fe868c9b89
Modupe Akanni <modupe775@gmail.com>
2023-05-06 11:58:55 +0100

    modify blog section

--- src/components/blog/Blog.jsx
+++ src/components/blog/Blog.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './blog.css'
 
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 
 // Article component
