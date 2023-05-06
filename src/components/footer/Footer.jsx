6b4b035dc15cb06ee69400b9a1899f16965056cf
Modupe Akanni <modupe775@gmail.com>
2023-05-06 13:14:09 +0100

    modify footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,6 +4,7 @@ import {FaFacebookF} from 'react-icons/fa'
 import {FiInstagram} from 'react-icons/fi'
 import {IoLogoTwitter} from 'react-icons/io'
 import {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
   return (
@@ -18,12 +19,14 @@ const Footer = () => {
         <li><a href="#portfolio">Portfolio</a></li>
         {/* <li><a href="#testimonials">Testimonials</a></li> */}
         <li><a href="#contact">Contact</a></li>
+        <li><a href="#blog">Blog</a></li>
       </ul>
 
       <div className="footer__socials">
         <a href="https://www.linkedin.com/in/modupe-akanni/"><FaLinkedin/></a>
         <a href="https://instagram.com/adegoke_akanni"><FiInstagram/></a>
         <a href="https://twitter.com/aka_goke"><IoLogoTwitter/></a>
+        <a href="https://github.com/Goketech"><FaGithub/></a>
       </div>
 
       <div className="footer__copyright">
