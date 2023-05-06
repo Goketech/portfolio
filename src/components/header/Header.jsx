61ed14111245a6033ed527fa97864bbe085a27d4
Modupe Akanni <modupe775@gmail.com>
2023-05-06 22:36:16 +0100

    modify header-image file path

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/modupe.jpg'
+import ME from '../../assets/modupe.png'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
